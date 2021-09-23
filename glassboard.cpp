a58ceda8146d8b1a741eca5266670a754b371343
YashGovindani <yashgovindani222@gmail.com>
2021-09-23 09:59:37 +0530

    set loading info

--- glassboard.cpp
+++ glassboard.cpp
@@ -10,10 +10,13 @@ GlassBoard * GlassBoard::glassBoard = nullptr;
 
 GlassBoard::GlassBoard(QWidget *loadingView):QLabel(nullptr)
 {
+    LoadingView *lv = (LoadingView *)loadingView;
+    lv->setInfo(QString("Initiating Glass Board"));
     this->setGeometry(QGuiApplication::primaryScreen()->geometry());
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::WindowDoesNotAcceptFocus | Qt::WindowTransparentForInput);
     this->setStyleSheet(QString("background-color : rgba(30, 30, 30, 100);"));
     this->setAttribute(Qt::WA_TranslucentBackground);
+    lv->setInfo(QString("Initiated Glass Board"));
 }
 
 GlassBoard *GlassBoard::get(QWidget *loadingView)
